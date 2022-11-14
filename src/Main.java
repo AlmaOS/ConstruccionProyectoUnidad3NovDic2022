813d3b14b7904d9fbe73260f1b9424c2cb96791c
Natali <a17001060@gmail.com>
2022-11-13 18:24:37 -0600

    Vista 1 - 1a versión
    Se implementó una vista dummie de la tabla con los botones

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Vista.vistaPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        vistaPrincipal vista = new vistaPrincipal();
+        vista.setVisible(true);
+    }
+}
