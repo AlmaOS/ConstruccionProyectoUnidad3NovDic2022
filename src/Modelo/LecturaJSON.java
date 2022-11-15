9e4d70ec1368a93526fbbe2ad2211c17693113f9
Alma Ordoñez <alma.ordonez2002@gmail.com>
2022-11-15 01:27:49 -0600

    Se agrega excepción
    Se agregó la excepción para verificar la estructura de los employees en el documento JSON

--- src/Modelo/LecturaJSON.java
+++ src/Modelo/LecturaJSON.java
@@ -9,10 +9,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Set;
 
 public class LecturaJSON {
     private String[][] employeeInfo;
     private JSONObject objetosDeJSON;
+    private String[] atributosGuardados;
     private int numEmployee;
 
 
@@ -25,6 +27,7 @@ public class LecturaJSON {
             JSONObject auxObjetosDeJSON = new JSONObject((Map) objJSON);
             System.out.println("Json del archivo:");
             objetosDeJSON = (JSONObject) auxObjetosDeJSON.get("employees");
+            guardarAtributosEmployee();
             System.out.println(objetosDeJSON);
             numEmployee = numEmployeeCount();
             employeeInfo = new String[numEmployee][4];
@@ -37,6 +40,46 @@ public class LecturaJSON {
             System.out.println("Error detectado: "+ e.getMessage());
         } catch (ParseException e){
             System.out.println("Estructura del JSON incorrecta");
+        }
+    }
+
+    private void guardarAtributosEmployee(){
+        JSONArray listEmployee =(JSONArray) objetosDeJSON.get("employee");
+        for(Object emp : listEmployee){
+            JSONObject auxEmp = (JSONObject) emp;
+            Set<String> atributos = auxEmp.keySet();
+            atributosGuardados = new String[atributos.size()];
+            try {
+                verificarAtributos(atributos);
+            } catch (MissingAttribute e) {
+                System.out.println("La estructura del employee en el archivo es incorrecta.");
+                System.exit(1);
+            }
+        }
+
+
+    }
+
+    public void verificarAtributos(Set<String> atributosEmp) throws MissingAttribute {
+        String[] defaultAtributos ={"firstName","lastName","photo","id"};
+        int numAtributos = 0;
+        int i = 0;
+        for ( String key : atributosEmp) {
+            atributosGuardados[i] = (String) key;
+            numAtributos = 0;
+            for(String atrib : defaultAtributos) {
+                for (String llave : atributosGuardados) {
+                    if(atrib.equals(llave)){
+                        numAtributos++;
+                        break;
+                    }
+                }
+            }
+            i++;
+        }
+
+        if(numAtributos!=4){
+            throw new MissingAttribute();
         }
     }
 
